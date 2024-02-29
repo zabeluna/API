712bbe070af468eb7181eeab7e1fcb577ed01129
Izabelle Luna <personalife503@gmail.com>
2024-02-29 16:10:47 -0300

    add new router

--- controllers/serviceController.js
+++ controllers/serviceController.js
@@ -38,10 +38,56 @@ const serviceController = {
       //resgatando serviço do banco
       const service = await ServiceModel.findById(id);
 
+      if (!service) {
+        res.status(404).json({ msg: "Serviço não encontrado." });
+        return;
+      }
+
       res.json(service);
     } catch (error) {
       console.log(error);
     }
+  },
+  //deletando serviços
+  delete: async (req, res) => {
+    try {
+      const id = req.params.id;
+      //identificando se o serviço existe
+      const service = await ServiceModel.findById(id);
+
+      if (!service) {
+        res.status(404).json({ msg: "Serviço não encontrado." });
+        return;
+      }
+
+      const deletedService = await ServiceModel.findByIdAndDelete(id);
+
+      res
+        .status(200)
+        .json({ deletedService, msg: "Serviço excluído com sucesso!" });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  //resgatando seviços(dados) e atualizando msgs
+  update: async (req, res) => {
+    const id = req.params.id;
+
+    const service = {
+      name: req.body.name,
+      description: req.body.description,
+      price: req.body.price,
+      image: req.body.image,
+    };
+    //put
+    const updateService = await ServiceModel.findByIdAndUpdate(id, service);
+
+    if (!updateService) {
+      res.status(404).json({ msg: "Serviço não encontrado." });
+      return;
+    }
+
+    res.status(200).json({ service, msg: "Serviço atualizado com sucesso!" });
   },
 };
 
